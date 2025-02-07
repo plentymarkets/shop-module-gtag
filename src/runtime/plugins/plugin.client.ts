de9f51d534ba7677a7ebf2b598bf603df0dbedc3
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2025-02-07 09:36:41 +0100

    Update plugin.client.ts

--- src/runtime/plugins/plugin.client.ts
+++ src/runtime/plugins/plugin.client.ts
@@ -1,9 +1,8 @@
 // Expose plugin types
 import type {} from 'nuxt/app'
 import type { ModuleOptions } from '../../module'
-import { defineNuxtPlugin, useGtag, useRuntimeConfig, watch } from '#imports'
-import { useCookieConsent } from '~/composables/useCookieConsent'
-import { useMakeOrder } from '~/composables/useMakeOrder'
+import { defineNuxtPlugin, useCookieConsent, useRuntimeConfig, watch } from '#imports'
+import { useGtag } from '../composables/useGtag'
 import { CookieName } from '../utils'
 
 export default defineNuxtPlugin({
@@ -40,27 +39,6 @@ export default defineNuxtPlugin({
           analytics_storage: 'denied',
         })
         disableAnalytics()
-      }
-    })
-
-    // Order Watcher
-    const { data: order } = useMakeOrder()
-    watch(order, (value) => {
-      if (value && value?.order?.id && consent.value) {
-        const totalVat = value.totals.vats.reduce((acc: number, vat: { value: number }) => acc + vat.value, 0)
-
-        gtag('event', 'purchase', {
-          transaction_id: value.order.id,
-          value: options.showGrossPrices ? value.totals.totalGross : value.totals.totalNet,
-          currency: value.totals.currency,
-          tax: totalVat,
-          shipping: options.showGrossPrices ? value.totals.shippingGross : value.totals.shippingNet,
-          items: value.order.orderItems.map((item: { id: number, orderItemName: string, quantity: number }) => ({
-            item_id: item.id,
-            item_name: item.orderItemName,
-            quantity: item.quantity,
-          })),
-        })
       }
     })
   },
