6e9ad557f9c73cba1d7347d6b44b275efe059f66
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2025-02-11 20:29:56 +0100

    feat: add events

--- src/runtime/plugins/plugin.client.ts
+++ src/runtime/plugins/plugin.client.ts
@@ -1,9 +1,17 @@
 // Expose plugin types
 import type {} from 'nuxt/app'
 import type { ModuleOptions } from '../../module'
-import { defineNuxtPlugin, useRuntimeConfig, watch, useCookieConsent, useRegisterCookie, useGtag } from '#imports'
-import { useMakeOrder } from '~/composables/useMakeOrder'
+import {
+  defineNuxtPlugin,
+  useRuntimeConfig,
+  watch,
+  useCookieConsent,
+  useRegisterCookie,
+  usePlentyEvent
+} from '#imports'
+import { useGtag } from '../composables/useGtag'
 import { CookieName } from '../utils'
+import { cartGetters, orderGetters } from '@plentymarkets/shop-api'
 
 export default defineNuxtPlugin({
   parallel: true,
@@ -42,26 +50,95 @@ export default defineNuxtPlugin({
       }
     })
 
-    // Order Watcher
-    const { data: order } = useMakeOrder()
-    watch(order, (value) => {
-      if (value && value?.order?.id && consent.value) {
-        const totalVat = value.totals.vats.reduce((acc: number, vat: { value: number }) => acc + vat.value, 0)
-
+    // Events
+    const { on } = usePlentyEvent();
+    on('frontend:orderCreated', (order) => {
+      if (consent.value && order.order && order.totals) {
+        const totalVat = order.totals.vats.reduce((acc: number, vat: { value: number }) => acc + vat.value, 0)
         gtag('event', 'purchase', {
-          transaction_id: value.order.id,
-          value: options.showGrossPrices ? value.totals.totalGross : value.totals.totalNet,
-          currency: value.totals.currency,
+          transaction_id: orderGetters.getId(order),
+          value: options.showGrossPrices ? order.totals.totalGross : order.totals.totalNet,
+          currency: order.totals.currency,
           tax: totalVat,
-          shipping: options.showGrossPrices ? value.totals.shippingGross : value.totals.shippingNet,
-          items: value.order.orderItems.map((item: { id: number, orderItemName: string, quantity: number }) => ({
-            item_id: item.id,
-            item_name: item.orderItemName,
-            quantity: item.quantity,
+          shipping: options.showGrossPrices ? order.totals.shippingGross : order.totals.shippingNet,
+          items: order.order.orderItems.map((item) => ({
+            item_id: orderGetters.getItemVariationId(item),
+            item_name: orderGetters.getItemName(item),
+            quantity: orderGetters.getItemQty(item),
+          })),
+        })
+      }
+    });
+
+    on('frontend:addToCart', (data) => {
+      if (consent.value) {
+        gtag('event', 'add_to_cart', {
+          items: [{
+            item_id: cartGetters.getVariationId(data.item),
+            item_name: cartGetters.getItemName(data.item),
+            quantity: data.addItemParams.quantity,
+          }],
+        })
+      }
+    })
+
+    on('frontend:removeFromCart', (data) => {
+      if (consent.value) {
+        gtag('event', 'remove_from_cart', {
+          items: [{
+            item_id: data.deleteItemParams.cartItemId,
+          }]
+        })
+      }
+    })
+
+    on('frontend:beginCheckout', (data) => {
+      if (consent.value) {
+        gtag('event', 'begin_checkout', {
+          currency: cartGetters.getCurrency(data),
+          value: options.showGrossPrices ? data.basketAmount : data.basketAmountNet,
+          items: data?.items?.map((item) => ({
+            item_id: cartGetters.getVariationId(item),
+            item_name: cartGetters.getItemName(item),
+            quantity: cartGetters.getItemQty(item),
           })),
         })
       }
     })
+
+    on('frontend:addToWishlist', (data) => {
+      if (consent.value) {
+        gtag('event', 'add_to_wishlist', {
+          items: [{
+            item_id: data.variationId,
+          }],
+        })
+      }
+    })
+
+    on('frontend:signUp', (data) => {
+      if (consent.value) {
+        gtag('event', 'sign_up', {
+          method: data.method,
+        })
+      }
+    });
+
+    on('frontend:login', (data) => {
+      if (consent.value) {
+        gtag('event', 'login', {
+          method: data.method,
+        })
+      }
+    });
+
+    on('frontend:searchProduct', (data) => {
+      if (consent.value) {
+        gtag('event', 'search', {
+          search_term: data,
+        })
+      }
+    });
 
     // Cookie Registration
     const { add } = useRegisterCookie();
